1198739ed5280215b5b17ef188978bd56acc7849
Your Name <you@example.com>
2019-11-23 11:14:52 +0530

    Updates

---
+++ precache-manifest.d93bfdbecfd6a57f5f209949ae41d3a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31214c7ea23fda005a655bc947487107",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "662da9db28202dd4f005",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "cbcfcd7c786a727d7607",
+    "url": "/robofriends/static/css/main.a1435eb4.chunk.css"
+  },
+  {
+    "revision": "662da9db28202dd4f005",
+    "url": "/robofriends/static/js/2.1fcf6901.chunk.js"
+  },
+  {
+    "revision": "cbcfcd7c786a727d7607",
+    "url": "/robofriends/static/js/main.5dbb9926.chunk.js"
+  },
+  {
+    "revision": "1b8f7e07790ae547720e",
+    "url": "/robofriends/static/js/runtime-main.4fd41d9f.js"
+  }
+]);
